79cd1478c519ab3f55c0385031b095985ae60f2f
micrana23 <micrana23@hotmail.com>
2025-01-29 16:32:41 +0100

    ImagenInsertada

--- frontend/nubia/src/components/Header.jsx
+++ frontend/nubia/src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
       <header className="relative">
-        <div className="h-48 bg-cover bg-center" style={{ backgroundImage: "url('/assets/images/background.jpg')" }}>
+        <div className="h-48 bg-cover bg-center" style={{ backgroundImage: "url('/assets/images/shishas.jpg')" }}>
           <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center">
             <h1 className="text-white text-3xl font-bold">Salón Nubia</h1>
             <p className="text-white text-sm mt-2">Yo voy a donde sea si es contigo hay shisha</p>
